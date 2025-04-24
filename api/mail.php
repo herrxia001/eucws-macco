f6747b694cc2352d0948c159574aea2a282698f9
Chaohui Xia <herrxia@gmail.com>
2025-04-24 16:40:04 +0200

    xrechnung macco

--- api/mail.php
+++ api/mail.php
@@ -14,16 +14,16 @@ function sendEmailSmtp($email, $body, $betreff) {
     $mail = new PHPMailer;
 
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'web143.alfahosting-server.de';  // Specify main and backup SMTP servers
+    $mail->Host = 'web152.alfahosting-server.de';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'info@shop-reho.com';                 // SMTP username
+    $mail->Username = 'info@moda-macco.com';                 // SMTP username
     $mail->Password = 'moda-prato23#';                           // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                    // TCP port to connect to
 
 
-    $sender = 'info@shop-reho.com';
-	$senderName = 'REHO';	
+    $sender = 'info@moda-macco.com';
+	$senderName = 'MACCO';	
 
     // Specify the SMTP settings.
     $mail->isSMTP();
